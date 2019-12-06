8d2562ea0f27f7b466bd7d5b6224b49b0ffd6466
yoshiaki.yatsu <hungry_soul_0122@yahoo.co.jp>
2019-12-06 08:04:33 +0000

    kadai-tasklist add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
+    
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
     <div class="row">
         <div class="col-6">
@@ -12,6 +14,11 @@
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
+                
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>                
         
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -13,6 +13,10 @@
             <th>タスク名</th>
             <td>{{ $task->content }}</td>
         </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $status->content }}</td>
+        </tr>        
     </table>
     
     {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
